e2285df6827b18c52a5e2671b72f7f59cd1910be
devJZen <quasar.joo@google.com>
2025-06-17 09:39:04 +0900

    build option change

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,13 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
+  build: {
+    sourcemap: false, // 소스맵 비활성화
+  },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
